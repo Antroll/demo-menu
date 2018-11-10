bc1a7912c31822a37ab5c83826a4ba2d1f5fa44f
Anton <panov-rabota@mail.ru>
2018-11-11 02:26:16 +0400

    styles and template

--- app/scripts/demo-menu.js
+++ app/scripts/demo-menu.js
@@ -6,6 +6,7 @@
 		// Create global element references
 		this.menu = null;
 		this.trigger = document.querySelector('.demo-menu__trigger');
+		this.overlay = document.querySelector('.demo-menu__overlay');
 
 
 		// Define option defaults
@@ -22,6 +23,7 @@
 	// Public Methods
 
 	DemoMenu.prototype.init = function() {
+		buildOut.call(this);
 		initializeEvents.call(this);
 	}
 
@@ -35,14 +37,62 @@
 	// Private Methods
 
 	function buildOut() {
+		document.querySelector('.demo-menu__thumb-wrap img').style.maxHeight = document.querySelector('.demo-menu__nav').clientHeight + 'px';
 	}
 
 	function initializeEvents() {
-
+		let timer
+		const menu = document.querySelector('.demo-menu')
+		const nav = document.querySelector('.demo-menu__nav')
 		if (this.trigger) {
 			this.trigger.addEventListener('click', function (e) {
 				e.preventDefault()
-				document.querySelector('.demo-menu').classList.toggle('demo-menu--active');
+				if (menu.classList.contains('demo-menu--active')) {
+					menu.classList.remove('demo-menu--active');
+				} else {
+					menu.classList.add('demo-menu--active');
+				}
+			});
+
+			menu.addEventListener('mouseenter', function () {
+				clearTimeout(timer)
+				timer = setTimeout(function() {
+					menu.classList.add('demo-menu--active');
+				}, 200);
+			});
+			menu.addEventListener('mouseleave', function () {
+				clearTimeout(timer)
+				timer = setTimeout(function() {
+					menu.classList.remove('demo-menu--active');
+				}, 2000);
+			});
+		}
+
+		if (this.overlay) {
+			this.overlay.addEventListener('click', function (e) {
+				e.preventDefault()
+				menu.classList.toggle('demo-menu--active');
+			});
+		}
+
+		const thumbIcons = document.querySelectorAll('.demo-menu__thumb-icon')
+		if (thumbIcons) {
+			for (var i = 0; i < thumbIcons.length; i++) {
+				loop()
+			}
+		}
+
+		const thumbWrap = document.querySelector('.demo-menu__thumb-wrap')
+		const thumb = thumbWrap.querySelector('img')
+		function loop() {
+			thumbIcons[i].addEventListener('mouseenter', function () {
+				const src = this.getAttribute('data-thumb')
+				thumbWrap.classList.add('demo-menu__thumb-wrap--active')
+				thumb.setAttribute('src', src)
+			});
+			thumbIcons[i].addEventListener('mouseleave', function () {
+				thumbWrap.classList.remove('demo-menu__thumb-wrap--active')
+				thumb.removeAttribute('src')
 			});
 		}
 
