ec6b8d2d8ddc45381142797e6025b8bc202c2b69
Anton <aliengrey88@gmail.com>
2018-12-27 12:36:27 +0400

    styles are taken now with СDN

--- app/scripts/demo-menu.js
+++ app/scripts/demo-menu.js
@@ -10,7 +10,7 @@
 		// Define option defaults
 		var defaults = {
 			template: '<div class="demo-menu"><div class="demo-menu__overlay"></div><div class="demo-menu__nav"><ul class="demo-menu__list demo-menu__list--custom-scroll"><li class="demo-menu__item" data-for="data-for"><a class="demo-menu__link" href="{{ href }}">{{ title }}<i class="demo-menu__thumb-icon" data-thumb="{{ thumb }}">?</i></a></li></ul><div class="demo-menu__thumb-wrap"><img alt=""></div><div class="demo-menu__trigger"><a class="demo-menu__trigger-btn" href="#"><i class="demo-menu__trigger-icon"></i></a></div></div></div>',
-			css: '//verstka.dev.magwai.ru/demo-menu/styles/demo-menu.css',
+			css: '//cdn.jsdelivr.net/gh/Antroll/demo-menu/dist/styles/demo-menu.css',
 			configPath: 'demo/data.json',
 		}
 
