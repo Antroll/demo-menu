e24233c542569288d3b255f1324f89c134dbeb7b
Anton <aliengrey88@gmail.com>
2018-12-27 17:15:03 +0400

    show menu after styles loaded

--- dist/scripts/demo-menu.js
+++ dist/scripts/demo-menu.js
@@ -10,11 +10,27 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 		// Create global element references
 		this.menu = null;
 
+		this.selectors = {
+			MENU: '.demo-menu',
+			TRIGGER: '.demo-menu__trigger',
+			OVERLAY: '.demo-menu__overlay',
+			NAV: '.demo-menu__nav',
+			THUMB_WRAP: '.demo-menu__thumb-wrap',
+			THUMB_ICON: '.demo-menu__thumb-icon'
+		};
+
+		this.states = {
+			MENU_ACTIVE: 'demo-menu--active',
+			MENU_INITIALIZED: 'demo-menu--inited',
+			THUMB_WRAP_ACTIVE: 'demo-menu__thumb-wrap--active'
+		};
+
 		// Define option defaults
 		var defaults = {
 			template: '<div class="demo-menu"><div class="demo-menu__overlay"></div><div class="demo-menu__nav"><ul class="demo-menu__list demo-menu__list--custom-scroll"><li class="demo-menu__item" data-for="data-for"><a class="demo-menu__link" href="{{ href }}">{{ title }}<i class="demo-menu__thumb-icon" data-thumb="{{ thumb }}">?</i></a></li></ul><div class="demo-menu__thumb-wrap"><img alt=""></div><div class="demo-menu__trigger"><a class="demo-menu__trigger-btn" href="#"><i class="demo-menu__trigger-icon"></i></a></div></div></div>',
 			css: '//cdn.jsdelivr.net/gh/Antroll/demo-menu/dist/styles/demo-menu.css',
-			configPath: 'demo/data.json'
+			configPath: 'demo/data.json',
+			activeOnHover: false
 
 			// Create options by extending defaults with the passed in arugments
 		};if (arguments[0] && _typeof(arguments[0]) === "object") {
@@ -27,7 +43,19 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 	////////////////////
 
 	DemoMenu.prototype.init = function () {
-		buildOut.call(this);
+		var self = this;
+		buildOut.call(self, afterBuild);
+
+		function afterBuild() {
+			initializeEvents.call(self);
+			stylesLoaded.call(self, afterStylesLoaded);
+		}
+
+		function afterStylesLoaded() {
+			var menu = document.querySelector(self.selectors.MENU);
+			menu.removeAttribute('style');
+			menu.classList.add(self.states.MENU_INITIALIZED);
+		}
 	};
 
 	DemoMenu.prototype.close = function () {
@@ -87,6 +115,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 		var list = itemTemplate.parentNode;
 		var pages = array;
 		template.querySelector('[data-for]').remove();
+		template.style.display = 'none';
 		for (var i = 0; i < pages.length; i++) {
 			var data = pages[i];
 
@@ -104,9 +133,8 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 	}
 
 	function addStyles(url) {
-		var css = 'h1 { background: red; }',
-		    head = document.head || document.getElementsByTagName('head')[0],
-		    link = document.createElement('link');
+		var head = document.head || document.getElementsByTagName('head')[0];
+		var link = document.createElement('link');
 
 		link.rel = 'stylesheet';
 		link.href = url;
@@ -114,8 +142,23 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 		head.appendChild(link);
 	}
 
-	function buildOut() {
-		var options = this.options;
+	function stylesLoaded(callback) {
+		var self = this;
+		var overlay = document.querySelector(self.selectors.OVERLAY);
+		var waiting = function waiting() {
+			var position = getComputedStyle(overlay).getPropertyValue('position');
+			if (position !== 'fixed') {
+				setTimeout(waiting, 200);
+			} else {
+				callback();
+			}
+		};
+		waiting();
+	}
+
+	function buildOut(callback) {
+		var that = this;
+		var options = that.options;
 
 		if (options.css) {
 			addStyles(options.css);
@@ -125,67 +168,74 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 			var json = JSON.parse(data);
 
 			parseTemplate(options.template, json.pages);
-			initializeEvents();
+
+			callback();
 		});
 	}
 
 	function initializeEvents() {
+		var self = this;
+		var options = self.options;
+		var trigger = document.querySelector(self.selectors.TRIGGER);
+		var overlay = document.querySelector(self.selectors.OVERLAY);
+		var menu = document.querySelector(self.selectors.MENU);
+		var nav = document.querySelector(self.selectors.NAV);
 		var timer = void 0;
-		var trigger = document.querySelector('.demo-menu__trigger');
-		var overlay = document.querySelector('.demo-menu__overlay');
-		var menu = document.querySelector('.demo-menu');
-		var nav = document.querySelector('.demo-menu__nav');
 		if (trigger) {
 			trigger.addEventListener('click', function (e) {
 				e.preventDefault();
-				if (menu.classList.contains('demo-menu--active')) {
-					menu.classList.remove('demo-menu--active');
+				if (menu.classList.contains(self.states.MENU_ACTIVE)) {
+					menu.classList.remove(self.states.MENU_ACTIVE);
 				} else {
-					menu.classList.add('demo-menu--active');
+					menu.classList.add(self.states.MENU_ACTIVE);
 				}
 			});
 
-			menu.addEventListener('mouseenter', function () {
-				clearTimeout(timer);
-				timer = setTimeout(function () {
-					menu.classList.add('demo-menu--active');
-				}, 400);
-			});
-			menu.addEventListener('mouseleave', function () {
-				clearTimeout(timer);
-				timer = setTimeout(function () {
-					menu.classList.remove('demo-menu--active');
-				}, 500);
-			});
+			if (options.activeOnHover) {
+
+				menu.addEventListener('mouseenter', function () {
+					clearTimeout(timer);
+					timer = setTimeout(function () {
+						menu.classList.add(self.states.MENU_ACTIVE);
+					}, 400);
+				});
+				menu.addEventListener('mouseleave', function () {
+					clearTimeout(timer);
+					timer = setTimeout(function () {
+						menu.classList.remove(self.states.MENU_ACTIVE);
+					}, 500);
+				});
+			}
 		}
 
 		if (overlay) {
 			overlay.addEventListener('click', function (e) {
 				e.preventDefault();
-				menu.classList.toggle('demo-menu--active');
+				menu.classList.toggle(self.states.MENU_ACTIVE);
 			});
 		}
 
-		var thumbIcons = document.querySelectorAll('.demo-menu__thumb-icon');
+		var thumbIcons = document.querySelectorAll(self.selectors.THUMB_ICON);
 		if (thumbIcons) {
+			var loop = function loop() {
+				thumbIcons[i].addEventListener('mouseenter', function () {
+					var src = this.getAttribute('data-thumb');
+					thumbWrap.querySelector('img').style.maxHeight = document.querySelector(self.selectors.NAV).clientHeight + 'px';
+					thumbWrap.classList.add(self.states.THUMB_WRAP_ACTIVE);
+					thumb.setAttribute('src', src);
+				});
+				thumbIcons[i].addEventListener('mouseleave', function () {
+					thumbWrap.classList.remove(self.states.THUMB_WRAP_ACTIVE);
+					thumb.removeAttribute('src');
+				});
+			};
+
 			for (var i = 0; i < thumbIcons.length; i++) {
 				loop();
 			}
-		}
 
-		var thumbWrap = document.querySelector('.demo-menu__thumb-wrap');
-		var thumb = thumbWrap.querySelector('img');
-		function loop() {
-			thumbIcons[i].addEventListener('mouseenter', function () {
-				var src = this.getAttribute('data-thumb');
-				thumbWrap.querySelector('img').style.maxHeight = document.querySelector('.demo-menu__nav').clientHeight + 'px';
-				thumbWrap.classList.add('demo-menu__thumb-wrap--active');
-				thumb.setAttribute('src', src);
-			});
-			thumbIcons[i].addEventListener('mouseleave', function () {
-				thumbWrap.classList.remove('demo-menu__thumb-wrap--active');
-				thumb.removeAttribute('src');
-			});
+			var thumbWrap = document.querySelector(self.selectors.THUMB_WRAP);
+			var thumb = thumbWrap.querySelector('img');
 		}
 	}
 
@@ -203,8 +253,9 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 ;(function () {
 	var url = document.querySelector('[data-demo-data]').getAttribute('data-demo-data');
 	var menu = new DemoMenu({
-		configPath: url
-		// css: false,
+		configPath: url,
+		activeOnHover: true
+		// css: 'styles/demo-menu.css',
 	});
 	menu.init();
 })();
